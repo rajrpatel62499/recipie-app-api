34c585be05202bf4e14329402aacb501ece45c5d
rajrpatel <raj.patel@alpsinfo.net>
2020-12-11 16:25:35 +0530

    made admin user fields compatible with custome user field

--- app/app/calc.py
+++
@@ -1,9 +0,0 @@
-
-def add(x, y):
-    """Add two number together"""
-    return x + y
-
-
-def substract(x, y):
-    """Substract two number y - x"""
-    return y - x
---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,36 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+    
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email = 'admin@admin.com',
+            password='admin'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='test@test.com',
+            password='test',
+            name="MyTestUser"
+        )
+        
+    def test_users_listed(self):
+        """Test that users are listed on user page"""
+        url = reverse('admin:core_user_changelist') # 'app:inbuilt'
+        print(url)
+        res = self.client.get(url)
+        
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+    
+    def test_user_change_page(self):
+        """Test that the user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        print(url)
+        res = self.client.get(url)
+        
+        self.assertEqual(res.status_code, 200)
